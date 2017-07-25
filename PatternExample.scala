7ac83f0c9bbb50bf8e8920c06ba8ade6fc2e31dd
liwangchun <toolpackage@163.com>
2017-07-25 16:06:21 -0400

    add regex

---
+++ PatternExample.scala
@@ -0,0 +1,18 @@
+import scala.util.matching.Regex
+//convert a string to regular expression by usring .r method
+val keyValPattern:Regex = "([0-9a-zA-Z-#() ]+): ([0-9a-zA-Z-#() ]+)".r
+//multi-line string with fixed alignment
+val input:String =
+"""background-color: #A03300;
+|background-image: url(img/header100.png);
+|background-position: top center;
+|background-repeat: repeat-x;
+|background-size: 2160px 108px;
+|margin: 0;
+|height: 108px;
+|width: 100%;
+""".stripMargin
+
+for (patternMatch <- keyValPattern.findAllMatchIn(input))
+  println(s"Key: ${patternMatch.group(1)} value:${patternMatch.group(2)}")
+
